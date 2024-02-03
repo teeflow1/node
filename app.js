862f73a7336394ae999f6d74de11c492d0a4cb4f
-add <temtopeayobami@gmail.com>
2024-02-03 10:00:13 +0100

    Done

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const http = require('http');
 
 
 
-const server = http.createServer((req, res) =>{
-    
-    if (req.url === '/'){
-        res.end('welcome to NodeJs App')
-    }
+const server = http.createServer ((req, res) =>{
 
-    if(req.url === '/about'){
-        res.end('This is the about page')
-    }
-    
-    //res.end('<h1>Oops!!!</h1>!  This page does not exist,  <a href="/"> Go back to home</a>')
+if (req.url === '/'){
+res.end('Welcome home')
+}
 
-    //console.log(req)
-    //res.write('Welcome To My First NodeJs App')
+if (req.url === '/about'){
+    res.end('This is the about page')
+}
+
+if(req.url === '/contact')
+{
+    res.end('This is the contact page')
+}
+
+
+
+    //res.write('Good Lord Man')
     //res.end()
 
 })
 
-server.listen(5000)
+server.listen(4000)
